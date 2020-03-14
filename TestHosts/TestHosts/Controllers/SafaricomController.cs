88ab0aa3ba95081e69c7e436f9ecdc80462af827
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2020-03-14 19:04:02 +0000

    Added safaricom test host

---
+++ TestHosts/TestHosts/Controllers/SafaricomController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestHosts.Controllers
+{
+    using System.IO;
+    using System.Xml;
+    using System.Xml.Serialization;
+    using Shared.Logger;
+
+    [Route("api/safaricom")]
+    [ApiController]
+    public class SafaricomController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> PerformTopup([FromQuery] String vendor, [FromQuery] String reqType, [FromQuery] String data)
+        {
+            // Deserialise the request message
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(data);
+            XmlSerializer xs = new XmlSerializer(typeof(SafaricomRequest));
+            SafaricomRequest cl = (SafaricomRequest)xs.Deserialize(new StringReader(doc.OuterXml));
+            
+            // Build the response object
+
+            OkObjectResult result = Ok(new SafaricomResponse
+                                       {
+                                           MESSAGE = this.GenerateTxnStatusMessage(cl.Amount),
+                                           DATE = cl.Date,
+                                           EXTREFNUM = cl.ExternalReferenceNumber,
+                                           TXNID = this.GenerateTransactionId(),
+                                           TXNSTATUS = this.GenerateTxnStatus(cl.Amount),
+                                           TYPE = "EXRCTRFRESP"
+            });
+
+            // currently result.Formatters is empty but we'd like to ensure it will be so in the future
+            result.Formatters.Clear();
+
+            // force response as xml
+            result.Formatters.Add(new Microsoft.AspNetCore.Mvc.Formatters.XmlSerializerOutputFormatter());
+
+            return result;
+        }
+
+        private String GenerateTransactionId()
+        {
+            return DateTime.Now.ToString($"yyyyMMddHHmmssfff");
+        }
+
+        private Int32 GenerateTxnStatus(Int32 transactionAmount)
+        {
+            if (transactionAmount == 25000)
+            {
+                return 400;
+            }
+
+            if (transactionAmount > 25000)
+            {
+                return 401;
+            }
+
+            if (transactionAmount < 1000)
+            {
+                return 402;
+            }
+
+            return 200;
+        }
+
+        private String GenerateTxnStatusMessage(Int32 transactionAmount)
+        {
+            if (transactionAmount == 25000)
+            {
+                return "Bad Request";
+            }
+
+            if (transactionAmount > 25000)
+            {
+                return "Amount Greater than 25000";
+            }
+
+            if (transactionAmount < 1000)
+            {
+                return "Amount Less than 1000";
+            }
+
+            return "Topup Successful";
+        }
+    }
+
+
+    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://safaricom.co.ke/Pinless/keyaccounts/")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://safaricom.co.ke/Pinless/keyaccounts/", IsNullable = false, ElementName = "COMMAND")]
+    public class SafaricomRequest
+    { 
+        [XmlElement(ElementName = "TYPE")]
+        public String Type { get; set; }
+
+        [XmlElement(ElementName = "DATE")]
+        public String Date { get; set; }
+
+        [XmlElement(ElementName = "EXTNWCODE")]
+        public String NetworkCode { get; set; }
+
+        [XmlElement(ElementName = "MSISDN")]
+        public String BankMSISDN { get; set; }
+
+        [XmlElement(ElementName = "PIN")]
+        public String Pin { get; set; }
+
+        [XmlElement(ElementName = "LOGINID")]
+        public String LOGINID { get; set; }
+
+        [XmlElement(ElementName = "PASSWORD")]
+        public String PASSWORD { get; set; }
+
+        [XmlElement(ElementName = "EXTCODE")]
+        public String ExternalBankCode { get; set; }
+
+        [XmlElement(ElementName = "EXTREFNUM")]
+        public String ExternalReferenceNumber { get; set; }
+
+        [XmlElement(ElementName = "MSISDN2")]
+        public String CustomerMSISDN { get; set; }
+
+        [XmlElement(ElementName = "AMOUNT")]
+        public Int32 Amount { get; set; }
+
+        [XmlElement(ElementName = "LANGUAGE1")]
+        public String BankLanguage { get; set; }
+
+        [XmlElement(ElementName = "LANGUAGE2")]
+        public String CustomerLanguage { get; set; }
+
+        [XmlElement(ElementName = "SELECTOR")]
+        public String Selector { get; set; }
+    }
+
+
+    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false, ElementName = "COMMAND")]
+    public partial class SafaricomResponse
+    {
+        [XmlElement(ElementName = "TYPE")]
+        public String TYPE { get; set; }
+
+        [XmlElement(ElementName = "TXNSTATUS")]
+        public Int32 TXNSTATUS { get; set; }
+
+        [XmlElement(ElementName = "DATE")]
+        public String DATE { get; set; }
+
+        [XmlElement(ElementName = "EXTREFNUM")]
+        public String EXTREFNUM { get; set; }
+
+        [XmlElement(ElementName = "TXNID")]
+        public string TXNID { get; set; }
+
+        [XmlElement(ElementName = "MESSAGE")]
+        public String MESSAGE { get; set; }
+    }
+
+
+}
---
+++ TestHosts/TestHosts/Startup.cs
@@ -0,0 +1,92 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NLog.Extensions.Logging;
+
+namespace TestHosts
+{
+    using System;
+    using System.IO;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.OpenApi.Models;
+    using Shared.Extensions;
+    using Shared.Logger;
+    using ILogger = Microsoft.Extensions.Logging.ILogger;
+
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment webHostEnvironment)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder().SetBasePath(webHostEnvironment.ContentRootPath)
+                                                                      .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                                                                      .AddJsonFile($"appsettings.{webHostEnvironment.EnvironmentName}.json", optional: true).AddEnvironmentVariables();
+
+            Startup.Configuration = builder.Build();
+            Startup.WebHostEnvironment = webHostEnvironment;
+        }
+
+        public static IConfigurationRoot Configuration { get; set; }
+
+        public static IWebHostEnvironment WebHostEnvironment { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+                                   {
+                                       c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                                   });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            String nlogConfigFilename = "nlog.config";
+
+            if (env.IsDevelopment())
+            {
+                nlogConfigFilename = $"nlog.{env.EnvironmentName}.config";
+                app.UseDeveloperExceptionPage();
+            }
+
+            loggerFactory.ConfigureNLog(Path.Combine(env.ContentRootPath, nlogConfigFilename));
+            loggerFactory.AddNLog();
+
+            ILogger logger = loggerFactory.CreateLogger("TestHosts");
+
+            Logger.Initialise(logger);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.AddRequestLogging();
+            app.AddResponseLogging();
+            app.AddExceptionHandler();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+                             {
+                                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                                 c.RoutePrefix = string.Empty;
+                             });
+        }
+    }
+}
