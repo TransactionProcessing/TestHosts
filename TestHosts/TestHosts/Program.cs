f0760afc15bdb06c8c9794cc023438366d890b2f
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2023-06-01 17:04:49 +0100

    :|

--- TestHosts/TestHosts/Program.cs
+++ TestHosts/TestHosts/Program.cs
@@ -20,9 +20,11 @@ namespace TestHosts
 
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
+            FileInfo fi = new FileInfo(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
             //At this stage, we only need our hosting file for ip and ports
-            IConfigurationRoot config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
-                                                                  .AddJsonFile("hosting.json", optional: true)
+            IConfigurationRoot config = new ConfigurationBuilder().SetBasePath(fi.Directory.FullName)
+                                                                  .AddJsonFile("hosting.json", optional: false)
                                                                   .AddJsonFile("hosting.development.json", optional: true)
                                                                   .AddEnvironmentVariables().Build();
 
